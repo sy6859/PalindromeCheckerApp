e0a347089c57913aef0190361fdd1fa8084a1a44
sy6859 <sy6859@srmist.edu.in>
2026-03-14 14:48:45 +0530

    Create UserPalindromeCheckerApp with welcome messages

---
+++ UserPalindromeCheckerApp.java
@@ -0,0 +1,17 @@
+/**
+ * ============================================================
+ * MAIN CLASS - UseCase1PalindromeCheckerApp
+ * ============================================================
+ *
+ * UC1: Application Entry & Welcome Message
+ */
+
+public class UseCasePalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
+    }
+}
